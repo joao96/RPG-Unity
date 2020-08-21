832cd577a86542b97ee4cdc960a34d457b9f3365
João Victor de Souza Poletti <jvpoletti@gmail.com>
2020-08-21 20:06:56 -0300

    Raycasting for components

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using RPG.Movement;
+using RPG.Combat;
 
 namespace RPG.Control
 {
@@ -13,25 +14,54 @@ namespace RPG.Control
         {
             mover = GetComponent<Mover>();
         }
+
         void Update()
+        {
+            InteractWithCombat();
+            InteractWithMovement();
+        }
+
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            
+            foreach (RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+
+                if (target == null) continue;
+                
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement()
         {
             if (Input.GetMouseButton(0))
             {
                 MoveToCursor();
             }
         }
+
         private void MoveToCursor()
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
 
-            bool hasHit = Physics.Raycast(ray, out hitInfo);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hitInfo);
 
             if (hasHit)
             {
 
                 mover.MoveTo(hitInfo.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }
 }
