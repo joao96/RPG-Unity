65d1abfb0b8a8ba44098a3238dbee08d1b61b532
jvpoletti <jvpoletti@gmail.com>
2020-08-21 18:02:36 -0300

    Basic Movement and setting of combat

---
+++ Assets/Scripts/Control/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using RPG.Movement;
+
+namespace RPG.Control
+{
+    public class PlayerController : MonoBehaviour
+    {
+
+        //cached
+        Mover mover;
+
+        void Start()
+        {
+            mover = GetComponent<Mover>();
+        }
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                MoveToCursor();
+            }
+        }
+        private void MoveToCursor()
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            bool hasHit = Physics.Raycast(ray, out hitInfo);
+
+            if (hasHit)
+            {
+
+                mover.MoveTo(hitInfo.point);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Movement/Mover.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace RPG.Movement
+{
+    public class Mover : MonoBehaviour
+    {
+        NavMeshAgent _myNavMeshAgent;
+        Animator _myAnimator;
+
+        void Start()
+        {
+            _myNavMeshAgent = GetComponent<NavMeshAgent>();
+            _myAnimator = GetComponent<Animator>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            _myNavMeshAgent.destination = destination;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = _myNavMeshAgent.velocity; // world space
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity); // local space
+            // z -> forward (it's what the blender tree uses to animate properly)!
+            float speed = localVelocity.z;
+            _myAnimator.SetFloat("forwardSpeed", speed);
+        }
+    }
+}
