62a784cc0da31c56b71e4957233b2335d829e1f8
João Victor de Souza Poletti <jvpoletti@gmail.com>
2020-08-23 19:58:37 -0300

    Triggering death

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,24 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour {
         
-        [SerializeField] float health = 10f;
+        [SerializeField] float healthPoints = 10f;
+
+        bool isDead = false;
 
         public void TakeDamage(float damage)
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            
+            if (healthPoints == 0 && !isDead)
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            GetComponent<Animator>().SetTrigger("die");
+            isDead = true;
         }
     }
 }
