4e90371eaa988143940d5fcb3dacf7968799a8e7
João Victor de Souza Poletti <jvpoletti@gmail.com>
2020-08-24 15:30:23 -0300

    AI chase distance

---
+++ Assets/Scripts/Control/AIController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace RPG.Control
+{
+    public class AIController : MonoBehaviour
+    {
+        [SerializeField] float chaseDistance = 5f;
+
+        private void Update()
+        {
+            if (PlayerIsInRange())
+            {
+                print("I " + gameObject.name + "Should chase!");
+            }
+
+        }
+
+        private bool PlayerIsInRange()
+        {
+            GameObject player = GameObject.FindWithTag("Player");
+            return Vector3.Distance(transform.position, player.transform.position) <= chaseDistance;
+        }
+
+        void OnDrawGizmos()
+        {
+            // Draw a blue sphere at the transform's position
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, chaseDistance);
+        }
+    }
+    
+}
